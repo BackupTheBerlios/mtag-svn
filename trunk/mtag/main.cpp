3c407cbb89aa14f9ece68fe5caede623a19f19df
flx <flx@10c4b9f8-0c3c-0410-8d01-87f850190063>
2007-10-10 05:37:48 +0000

    getTags, setTags, addtag is working

--- trunk/mtag/main.cpp
+++ trunk/mtag/main.cpp
@@ -19,7 +19,7 @@
 
 using namespace std;
 
-int getTag(char* filename, vector<TagLib::String> tags)
+int getTags(char* filename, vector<TagLib::String> *tags)
 {
 	TagLib::FileRef f(filename);
 	if(!f.isNull() && f.tag())
@@ -34,23 +34,35 @@ int getTag(char* filename, vector<TagLib::String> tags)
 			int tagEnd = sTags.find(",", tagStart);
 			if (tagEnd < 0) tagEnd = 0xffffffff;
 			TagLib::String thisTag = sTags.substr(tagStart, tagEnd - tagStart);
-			tags.insert(tags.begin(), thisTag);
-			cout << thisTag << endl;
+			tags->insert(tags->end(), thisTag);
 		}
-		
+		return EXIT_SUCCESS;
 	}
-	return EXIT_SUCCESS;
+	return -1;
 }
 
-int addtag(char* filename, char* tag)
+int setTags(char* filename, vector<TagLib::String> *tags)
 {
 	TagLib::FileRef f(filename);
 	if(!f.isNull() && f.tag())
 	{
 		TagLib::Tag *tag = f.tag();
-		TagLib::String tags = tag->comment();
+
+		TagLib::String sTags;
+		for (vector<TagLib::String>::iterator it = tags->begin(); it!=tags->end(); ++it)
+		{
+			if (it != tags->begin())
+				sTags.append(", ");
+			sTags.append("tag:");
+			sTags.append(*it);
+		}
+
+		tag->setComment(sTags);
+		f.save();
+
+		return EXIT_SUCCESS;
 	}
-	return EXIT_SUCCESS;
+	return -1;
 }
 
 int main(int argc, char *argv[])
@@ -62,6 +74,14 @@ int main(int argc, char *argv[])
 	switch(optchar)
 	{
         	case 'a':
+			for(int i = 1; i < argc; i++)
+			{
+				vector<TagLib::String> tags;
+				if (getTags(argv[i], &tags) < 0)
+					continue;
+				tags.insert(tags.end(), optarg);
+				setTags(argv[i], &tags);
+			}
 			break;
 	}
 
@@ -69,11 +89,17 @@ int main(int argc, char *argv[])
 	for(int i = 1; i < argc; i++)
 	{
 		vector<TagLib::String> tags;
-		getTag(argv[i], tags);
-		for (int i = 0; i <= tags.size(); i++)
+		if (getTags(argv[i], &tags) < 0)
+			continue;
+		cout << "track: " << argv[i] << endl;
+		cout << "tags: ";
+		for (vector<TagLib::String>::iterator it = tags.begin(); it!=tags.end(); ++it)
 		{
-			cout << tags[i] << endl;
+			if (it != tags.begin())
+				cout << ", ";
+			cout << *it;
 		}
+		cout << endl;
 	}
 
 	return EXIT_SUCCESS;
