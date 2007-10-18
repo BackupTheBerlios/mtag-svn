54cbe489015cec3c71c8e9596fdf225cec238791
flx <flx@10c4b9f8-0c3c-0410-8d01-87f850190063>
2007-10-18 10:15:09 +0000

    fix script

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,25 +7,28 @@ echo making release: ${RELEASE}
 
 (cd trunk/mtag ; make clean)
 svn cp trunk/mtag tags/mtag-${RELEASE}
-svn --force rm mtag-${RELEASE}/*~
-svn --force rm mtag-${RELEASE}/*.mp3
-svn --force rm mtag-${RELEASE}/*.ogg
-svn --force rm mtag-${RELEASE}/CMakeCache.txt
-sed -e 's/-Wall ?//' -e 's/CMAKE_VERBOSE_MAKEFILE ON/CMAKE_VERBOSE_MAKEFILE OFF/' -i mtag-${RELEASE}/CMakeLists.txt
+svn --force rm tags/mtag-${RELEASE}/*~
+svn --force rm tags/mtag-${RELEASE}/*.mp3
+svn --force rm tags/mtag-${RELEASE}/*.ogg
+svn --force rm tags/mtag-${RELEASE}/*.sq3
+svn --force rm tags/mtag-${RELEASE}/CMakeCache.txt
+sed -e 's/-Wall ?//' -e 's/CMAKE_VERBOSE_MAKEFILE ON/CMAKE_VERBOSE_MAKEFILE OFF/' -i tags/mtag-${RELEASE}/CMakeLists.txt
 
 #svn ci -m "release ${RELEASE}"
 cp -r tags/mtag-${RELEASE} tars/mtag-${RELEASE}
-cd tars/mtag-${RELEASE}
-rm -rf .??*
-rm -rf *.kdevelop* templates
-cd ..
+
+rm -rf tars/mtag-${RELEASE}/.??*
+rm -rf tars/mtag-${RELEASE}/*.kdevelop*
+rm -rf tars/mtag-${RELEASE}/templates
+find tars/mtag-${RELEASE}/ -name .svn -exec rm -rf {} \;
+
+cd tars
 tar -czf mtag-${RELEASE}.tar.gz mtag-${RELEASE}
 gpg -b mtag-${RELEASE}.tar.gz
 svn add mtag-${RELEASE}.tar.gz*
 rm -rf mtag-${RELEASE}
-
 cd ..
-svn cp ebuilds/mtag-base.ebuild ebuilds/mtag-${RELEASE}.ebuild
 
+svn cp ebuilds/mtag-base.ebuild ebuilds/mtag-${RELEASE}.ebuild
 
 #svn ci -m "release ${RELEASE}"
