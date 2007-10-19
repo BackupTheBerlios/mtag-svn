bc6fd41f150bc29494eb1bda1c03add3b5bebc26
flx <flx@10c4b9f8-0c3c-0410-8d01-87f850190063>
2007-10-19 10:52:56 +0000

    release 1

---
+++ ebuilds/mtag-1.ebuild
@@ -0,0 +1,31 @@
+# Copyright 1999-2007 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+# $Header: $
+
+inherit eutils
+
+DESCRIPTION="the fast media tag lib"
+HOMEPAGE="http://mtag.berlios.de/"
+SRC_URI="http://svn.berlios.de/svnroot/repos/mtag/tars/${P}.tar.gz
+	http://download.berlios.de/mtag/${P}.tar.gz
+	http://download2.berlios.de/mtag/${P}.tar.gz"
+
+LICENSE="GPL-2"
+SLOT="0"
+KEYWORDS="~x86 ~amd64"
+IUSE=""
+
+DEPEND="dev-util/cmake
+	media-libs/taglib
+	>=dev-db/sqlite-3.3.12"
+
+src_unpack() {
+	unpack ${A}
+	cd ${S}
+	cmake ${S}
+}
+
+src_install() {
+	[ ! -d "${D}/usr/bin/" ] && mkdir -p ${D}/usr/bin/
+	cp ${S}/mtag ${D}/usr/bin/ || die "install failed"
+}
---
+++ tags/mtag-1/db.cpp
@@ -0,0 +1,117 @@
+//
+// C++ Implementation: db
+//
+// Description: 
+//
+//
+// Author: Felix Bechstein <f@ub0r.de>, (C) 2007
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+
+#include <stdlib.h>
+#include <tag.h>
+
+/* For sqlite3 */
+#include <sqlite3.h>
+
+
+/* For taglib */
+#include <fileref.h>
+
+#include "db.h"
+
+using namespace std;
+
+int search_callback(void *filesp, int argc, char **argv, char **colnames)
+{
+	TagLib::StringList *files = (TagLib::StringList *) filesp;
+	for (int i = 0; i < argc; i++)
+	{
+		files->append(argv[i]);
+	}
+	return EXIT_SUCCESS;
+}
+
+
+int sql::openDB(sqlite3 **db)
+{
+	if(sqlite3_open(database, db) != SQLITE_OK)
+	{
+		return EXIT_FAILURE;
+	}
+	sqlite3_exec(*db, "CREATE TABLE tags (filename TEXT, tag TEXT);", NULL, NULL, NULL);
+	sqlite3_exec(*db, "CREATE INDEX tagsByFile ON tags(filename ASC);", NULL, NULL, NULL);
+	sqlite3_exec(*db, "CREATE INDEX tagsByTags ON tags(tag ASC);", NULL, NULL, NULL);
+	return EXIT_SUCCESS;
+}
+
+int sql::closeDB(sqlite3* db)
+{
+	return sqlite3_close(db);
+}
+
+int sql::addTag(const char* filename, const char* tag, sqlite3* db)
+{
+	string s("INSERT INTO tags(filename, tag) VALUES('");
+	s.append(filename);
+	s.append("', '");
+	s.append(tag);
+	s.append("');");
+	return sqlite3_exec(db, s.c_str(), NULL, NULL, NULL);
+}
+
+int sql::addTag(const char* filename, const char* tag)
+{
+	sqlite3* db;
+	if(openDB(&db) != EXIT_SUCCESS)
+		return EXIT_FAILURE;
+	int res = addTag(filename, tag, db);
+	closeDB(db);
+	return res;
+}
+
+int sql::clearTags(const char* filename, sqlite3* db)
+{
+	string s("DELETE FROM tags WHERE filename='");
+	s.append(filename);
+	s.append("';");
+	return sqlite3_exec(db, s.c_str(), NULL, NULL, NULL);
+}
+
+int sql::clearTags(const char* filename)
+{
+	sqlite3* db;
+	if(openDB(&db) != EXIT_SUCCESS)
+		return EXIT_FAILURE;
+	int res = clearTags(filename, db);
+	closeDB(db);
+	return res;
+}
+
+
+int sql::search(const char* tag, TagLib::StringList *files, sqlite3* db)
+{
+	string s("SELECT DISTINCT filename FROM tags WHERE tag='");
+	s += tag;
+	s += "';";
+	return sqlite3_exec(db, s.c_str(), search_callback, files, NULL);
+}
+
+int sql::search(const char* tag, TagLib::StringList *files)
+{
+	sqlite3* db;
+	if(openDB(&db) != EXIT_SUCCESS)
+		return EXIT_FAILURE;
+	int res = search(tag, files, db);
+	closeDB(db);
+	return res;
+}
+
+const char *sql::database;
+
+void sql::setDataBase(const char *filename)
+{
+	database = filename;
+}
